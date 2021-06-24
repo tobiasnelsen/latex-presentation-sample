d567790b037b2716e64ae867faea0b5e4a144dd0
Tobias Nelsen <86366435+tobiasnelsen@users.noreply.github.com>
2021-06-24 20:21:42 +0200

    Update presentation

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
+IF EXIST presentation.pdf del presentation.pdf
 IF EXIST presentation_tmp RMDIR /S /Q presentation_tmp
 ruby C:\Users\User\Documents\GitHub\ltx2any\ltx2any.rb presentation.tex
+start presentation.pdf
 pause
